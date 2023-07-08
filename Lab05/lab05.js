c51ab92f45edaf17e5d2eeabdc52d98ee2beec08
hiusnguyen201 <hiusnguyen201@gmail.com>
2023-07-08 14:25:50 +0700

    Exercise Lab05

---
+++ Lab05/lab05.js
@@ -0,0 +1,14 @@
+var content = `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi minima placeat fuga quo consectetur molestias doloribus, suscipit nobis hic perspiciatis rerum sint voluptatem debitis facere dolorem labore commodi earum laudantium?`;
+
+document.write(content);
+
+var words = content.split(" ");
+
+var html = '';
+
+for(var i = 0; i < splitContent.length; i++)
+{
+    html += "<span>" + words[i] + "</span>";
+}
+
+console.log(splitContent);
