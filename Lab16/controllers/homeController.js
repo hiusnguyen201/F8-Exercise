135fab59861cdc115cac6f772c9bba8f37f3d248
hiusnguyen201 <hiusnguyen201@gmail.com>
2023-08-16 14:39:09 +0700

    Fix session

--- Lab16/controllers/homeController.js
+++ Lab16/controllers/homeController.js
@@ -1,21 +1,17 @@
 module.exports = {
     index: (req, res) => {
-        console.log(req.session.isLogged);
         if (req.session.isLogged) 
         {
             return res.render('home/index');
         } 
         else 
         {
-            req.session.isLogged = false;
-            console.log(req.session.isLogged);
             return res.redirect('/dang-nhap');
         }
     },
 
     handleLogout: (req, res) => {
         req.session.isLogged = false;
-        console.log(req.session.isLogged);
         req.flash('success', "Đăng xuất thành công");
         return res.redirect("/dang-nhap");
     }
